549af63fdb93122cc118755eb2549817ffae2bc0
Divyansh Pal <132265007+divyansh-07p@users.noreply.github.com>
2025-12-08 01:54:15 +0530

    Update MainLayout.tsx

--- src/components/Layout/MainLayout.tsx
+++ src/components/Layout/MainLayout.tsx
@@ -165,14 +165,7 @@ export default function MainLayout() {
         )}
       </main>
 
-      <footer className="bg-white border-t border-gray-200 mt-12">
-        <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-6">
-          <p className="text-center text-sm text-gray-600">
-            Made by Divyansh, Varshil, Shweta and Nirbhay for Tata Capital and EY Techathon &copy;{' '}
-            {new Date().getFullYear()}
-          </p>
-        </div>
-      </footer>
+    
     </div>
   );
 }
